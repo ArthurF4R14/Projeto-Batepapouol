6dc3ec7b75385fcd397c83bf3142390bb8509317
Arthur Faria <arthurfaria79@gmail.com>
2025-04-23 19:20:38 -0300

    Add files via upload

---
+++ projeto batepapo uol/script/variaveis-globais.js
@@ -0,0 +1,12 @@
+let mensagens =[]
+let usuarios =[];
+let usuariosBarraLateral = [];
+let postUser = {
+    name: null
+}
+
+let contatoPrivado = {
+    name:null
+}
+
+let mensagemPrivada = false;
